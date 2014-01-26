2649eb141f2fea2d3f90d7941e3cfa2e4f9146b4
Fabian Becker <halfdan@xnorfz.de>
2014-01-26 01:26:28 +0100

    Adding more specs using webmock

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,8 +1,34 @@
 require 'spec_helper'
 
 describe Instapush::Application do
+  before(:each) do
+    @event = Instapush::Event.new 'event'
+  end
+
   it 'should fail on invalid push argument' do
-    app = Instapush::Application.new 'key', 'secret'
+    app = Instapush::Application.new 'id', 'secret'
     lambda { app.push('foo')}.should raise_error(ArgumentError)
+  end
+
+  it 'should send the event name in the body' do
+    app = Instapush::Application.new 'id', 'secret'
+    stub_request(:post, "api.instapush.im/post").with(:body => "{\"event\":\"event\",\"trackers\":{}}")
+
+    app.push @event
+  end
+
+  it 'should send the app id as POST header' do
+    app = Instapush::Application.new 'id', 'secret'
+    stub_request(:post, "api.instapush.im/post").with(:body => "{\"event\":\"event\",\"trackers\":{}}", :headers => { 'X-INSTAPUSH-APPSECRET' => 'secret' })
+
+    app.push @event
+    
+  end
+
+  it 'should send the app secret as POST header' do
+    app = Instapush::Application.new 'id', 'secret'
+    stub_request(:post, "api.instapush.im/post").with(:body => "{\"event\":\"event\",\"trackers\":{}}", :headers => { 'X-INSTAPUSH-APPID' => 'id' })
+
+    app.push @event
   end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,4 +5,12 @@ describe Instapush::Event do
     event = Instapush::Event.new 'event'
     event.name.should eq('event')
   end
+
+  it 'should accept a hash of trackers' do
+    event = Instapush::Event.new 'event'
+    data = { a: 5, b: 7 }
+    event.tracker= data
+
+    event.tracker.should equal(data)
+  end
 end
